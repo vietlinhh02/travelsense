ee5464ef1d4b2a614c8b7a1370478889b1515a2b
Viet Linh <nvlinh0607@gmail.com>
2025-08-26 02:10:53 +0700

    feat(geminiService): remove unused service imports to streamline module exports

--- src/services/ai/index.js
+++ src/services/ai/index.js
@@ -1,16 +1,8 @@
 const GeminiService = require('./gemini.service');
-const GeminiApiClient = require('./geminiApiClient');
-const PromptBuilder = require('./promptBuilder');
-const ResponseParser = require('./responseParser');
-const ActivityTemplateService = require('./activityTemplateService');
 
 // Create instance of GeminiService
 const geminiService = new GeminiService();
 
 module.exports = {
-  geminiService,
-  GeminiApiClient,
-  PromptBuilder,
-  ResponseParser,
-  ActivityTemplateService
+  geminiService
 };
